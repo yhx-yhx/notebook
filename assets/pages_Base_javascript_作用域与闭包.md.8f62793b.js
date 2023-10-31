f9d5d4fa93341ae5c92dc034d2ced65d06a971aa
yhx-yhx <yhx-yhx@users.noreply.github.com>
2023-10-31 09:32:16 +0000

    deploy: 9695b1419040fe5cce6c4eef8cd6d53bf4cc531d

---
+++ assets/pages_Base_javascript_作用域与闭包.md.8f62793b.js
@@ -0,0 +1,6 @@
+import{_ as t,o as s,c as n,k as e,a}from"./chunks/framework.be66a235.js";const k=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"pages/Base/javascript/作用域与闭包.md","filePath":"pages/Base/javascript/作用域与闭包.md","lastUpdated":1698744301000}'),o={name:"pages/Base/javascript/作用域与闭包.md"},r=e("h2",{id:"块级作用域-函数作用域",tabindex:"-1"},[a("块级作用域 函数作用域 "),e("a",{class:"header-anchor",href:"#块级作用域-函数作用域","aria-label":'Permalink to "块级作用域 函数作用域"'},"​")],-1),c=e("pre",null,[e("code",null,`es5 是没有块级作用域的 
+使用函数作用域 闭包 来变更变量指向
+使用闭包 来解决问题 
+`)],-1),d=e("h2",{id:"闭包",tabindex:"-1"},[a("闭包 "),e("a",{class:"header-anchor",href:"#闭包","aria-label":'Permalink to "闭包"'},"​")],-1),_=e("pre",null,[e("code",null,`好处：数据私有 延长作用域
+不足：不合理使用 会导致严重内存泄露问题  定时器 监听 等行为 不急时清除  
+`)],-1),i=[r,c,d,_];function l(p,h,m,f,u,x){return s(),n("div",null,i)}const v=t(o,[["render",l]]);export{k as __pageData,v as default};
